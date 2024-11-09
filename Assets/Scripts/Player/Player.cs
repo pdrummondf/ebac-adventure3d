47453aa1ce80716bae450f4c0bc5a772627c9322
pdrummondf <pdrummondf@gmail.com>
2024-11-09 15:32:04 -0300

    Sexto commit

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,10 +9,14 @@ public class Player : MonoBehaviour
     [SerializeField] private float _gravity = 9.8f;
     [SerializeField] private float _jumpForce = 1f;
     [SerializeField] private float _vSpeed = 0f;
+    [SerializeField] private float _runSpeedFactor = 1f;
 
     [SerializeField] private Animator _animator;
+    [SerializeField] private KeyCode _runKey = KeyCode.LeftShift;
 
     private CharacterController _characterController;
+
+    private float _runSpeed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +27,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(_runKey))
+        {
+            _runSpeed = _runSpeedFactor;
+            _animator.speed = _runSpeedFactor;
+        }
+        else
+        {
+            _runSpeed = 1;
+            _animator.speed = 1;
+        }
+
         transform.Rotate(0, Input.GetAxis("Horizontal") * _turnSpeed * Time.deltaTime, 0);
 
         var verticallInput = Input.GetAxis("Vertical");
-        Vector3 _movementVector = transform.forward * _speed * verticallInput;
+        Vector3 _movementVector = transform.forward * _speed * verticallInput * _runSpeed;
 
         _animator.SetBool("Run", verticallInput != 0);
 
